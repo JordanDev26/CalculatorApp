4403b2cf7531e3ba54a2332df8e941367e8509b0
JordanDev26 <jrescalantedev@gmail.com>
2023-10-09 22:51:42 -0600

    First test

---
+++ src/org/jrescalante/calculatorapp/Main.java
@@ -0,0 +1,15 @@
+package org.jrescalante.calculatorapp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("**** Calculator App ****");
+        var value1 = 10;
+        var value2 = 20;
+        var result = value1 + value2;
+        System.out.println("result = " + result);
+    }
+
+    private static String showMenu(){
+        return "";
+    }
+}
